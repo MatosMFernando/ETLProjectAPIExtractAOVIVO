eb3ca4b0a93931eec78441ab8887cddfd68a1a99
Fernando Matos <fernandommatos@outlook.com>
2024-12-19 13:18:06 -0300

    update

--- src/pipeline_00.py
+++ src/pipeline_00.py
@@ -6,4 +6,21 @@ def extract_dados_bitcoin():
     response = requests.get(url)
     return response.json()
 
-print(extract_dados_bitcoin())
+def transform_dados_bitcoin(dados):
+    valor = dados['data']['amount']
+    currency = dados['data']['base']
+    moeda = dados['data']['currency']
+
+    dados_transformados = {
+        'valor': valor,
+        'currency': currency,
+        'moeda': moeda
+    }
+
+    return dados_transformados
+
+if __name__ == "__main__":
+    dados_json = extract_dados_bitcoin()
+    dados_transformados = transform_dados_bitcoin(dados_json)
+    print(dados_transformados)
+
